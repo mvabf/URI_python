d58fc08a2b676e74926e2137f550f4740edbfbfd
Marcus Venicius Baltazar <45337824+mvabf@users.noreply.github.com>
2020-02-19 22:03:40 -0300

    Create ex_1037.py

---
+++ ex_1037.py
@@ -0,0 +1,15 @@
+n = float(input())
+
+if n >= 0:
+    if n <= 25:
+        print("Intervalo [0,25]")
+    elif n <= 50:
+        print("Intervalo (25,50]")
+    elif n <= 75:
+        print("Intervalo (50,75]")
+    elif n <= 100:
+        print("Intervalo (75,100]")
+    else:
+        print("Fora de intervalo")
+else:
+    print("Fora de intervalo")
