984bc24980591d773e995c42928c6fd62cd7c299
Marcus Venicius Baltazar <45337824+mvabf@users.noreply.github.com>
2020-02-20 12:26:15 -0300

    Create ex_1035.py

---
+++ ex_1035.py
@@ -0,0 +1,11 @@
+a, b, c, d = input().split(" ")
+
+A = int(a)
+B = int(b)
+C = int(c)
+D = int(d)
+
+if B > C and D > A and C + D > A + B and C >= 0 and D >= 0 and A % 2 == 0:
+    print("Valores aceitos")
+else:
+    print("Valores nao aceitos")
