ab6a4f2fd916171f5ea750c4ccd60913151a152f
Marcus Venicius Baltazar <45337824+mvabf@users.noreply.github.com>
2020-03-07 12:00:10 -0300

    Create ex_1064.py

---
+++ ex_1064.py
@@ -0,0 +1,13 @@
+positivos = 0
+soma = 0
+for i in range(6):
+    n = float(input())
+    if n > 0:
+        positivos += 1
+        soma += n
+
+
+media = soma / positivos
+
+print("{} valores positivos".format(positivos))
+print("%.1f" % media)
