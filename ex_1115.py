24d4113a7d77855e327efced0f83f96d2aae0e94
Marcus Venicius Baltazar <45337824+mvabf@users.noreply.github.com>
2020-03-17 23:09:25 -0300

    Create ex_1115.py

---
+++ ex_1115.py
@@ -0,0 +1,16 @@
+x = 1
+y = 1
+
+while x != 0 and y != 0:
+    x, y = map(int, input().split())
+
+    if x > 0 and y > 0:
+        print("primeiro")
+    elif x < 0 and y > 0:
+        print("segundo")
+    elif x < 0 and y < 0:
+        print("terceiro")
+    elif x > 0 and y < 0:
+        print("quarto")
+    else:
+        quit()
